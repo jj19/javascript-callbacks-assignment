2070706db0877ef28136eeb1cb90f5826ca2c0e6
Jose "JJ" Ortiz <jortiz00@gmail.com>
2023-07-06 19:03:24 -0700

    Add changes as directed in the exercises

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 // Form validation, the process of ensuring a user's answers to a form are correct, complete, and fit within
-// some specific criteria (e.g. a password having containing a number or special character), is a VERY
-// important part of being a fullstack developer as it helps prevent errors, improve data accuracy, and ensures
-// that no data is missing upon form completion
+// some specific criteria (e.g. a password having containing a number or special character), is a VERY important part of being a fullstack developer as it helps prevent errors, improve data accuracy, and ensures that no data is missing upon form completion
 
 // Your assignment is to do the following:
-// 1. Write a function called validateForm(e) where e is an 'event' variable (this is default
-//    behavior for forms - feel free to call it 'event' as well)
+// 1. Write a function called validateForm(e) where e is an 'event' variable (this is default behavior for forms - feel free to call it 'event' as well)
 // 2. Inside of validateForm(e), get the information of the form fields by either using querySelectors
 //    or by traversing the event object and do the following checks:
 //      2a. If the #name input element is empty, send an alert telling the user to enter their name
@@ -23,3 +20,32 @@
 //Hint: When dealing with a form submission that takes an event variable as a parameter, use e.preventDefault()
 //      (or event.preventDefault() depending on the name of the variable) to prevent the page
 //      from refreshing when a form is submitted. Do all your checks after that line.
+
+function validateForm(e){
+  e.preventDefault()
+  let name = document.querySelector('#name').value
+  let email =document.querySelector('#email').value
+  let password = document.querySelector('#password').value
+  let regex = /@/
+  
+  
+        if (name === '') {
+          alert('Please enter your name')
+          return false
+        } else if (regex.test(email) == false) {
+          alert('Please enter a valid email')
+            return false
+          } else if (password == '') {
+            alert('Please enter your password')
+            return false
+          } else if (password.length < 8) {
+            alert('Password needs to be at least 8 characters long')
+            return false
+          } else {
+            return true
+          }
+
+        }
+
+let userForm = document.querySelector('#userForm')
+userForm.addEventListener('submit', validateForm)
